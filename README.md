ea671ed401727a2181de9e1802060230c93609a9
Erik Pejka <epejka@localhost.localdomain>
2014-02-01 22:13:56 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+sublime-text-installer
+======================
+
+Easy way to install Sublime Text 2 &amp; 3, and also to make backups of your own configs and settings to move to another computer.
+
---
+++ get-sublime.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Define the current install directory (default: /usr/local/bin)
+LOC_BIN=/usr/local/bin
+
+# Define where the configurations are stored (default: ~/.config)
+CFG_DIR=~/.config
+
+# Get the binaries
+echo "* Getting the binaries from: $LOC_BIN"
+mkdir -p bin
+cp -rf "$LOC_BIN/Sublime Text 3" 'bin/Sublime Text 2'
+cp -rf "$LOC_BIN/Sublime Text 3" 'bin/Sublime Text 3'
+
+# Get the config files
+echo "* Getting configs from: $CFG_DIR"
+mkdir -p config
+cp -rf "$CFG_DIR/sublime-text-2" config/sublime-text-2
+cp -rf "$CFG_DIR/sublime-text-3" config/sublime-text-3
